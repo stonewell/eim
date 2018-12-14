06d28ff444b1fbb71cc8d0774390bb852a274cdf
Jingnan Si <user.email>
2018-12-14 15:54:35 -0800

    add json interface library

--- src/commands/get_buffer_cmd.cxx
+++ src/commands/get_buffer_cmd.cxx
@@ -1,6 +1,8 @@
 #include "eim_context.h"
 #include "cmd_base.h"
 
+#include "tao/json.hpp"
+
 namespace eim
 {
 class GetBufferCommand : public virtual CommandBase {
@@ -13,7 +15,13 @@ public:
 
 public:
     virtual std::string Execute(EIMContextPtr context, const std::string & json_args) {
-        context->GetBufferManager()->GetBuffer(L"", true);
+        const tao::json::value v = tao::json::from_string(json_args);
+
+        auto buf_name = v.optional<std::string>("name");
+        auto file_path = v.optional<std::string>("path");
+        auto create = v.optional<bool>("create");
+
+        context->GetBufferManager()->GetBuffer(L"", create.has_value() ? create.value() : true);
 
         return json_args;
     }
