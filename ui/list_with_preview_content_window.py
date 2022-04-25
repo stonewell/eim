30928d13a21aaf9aa8f49caa274998968bd96a80
Jingnan Si <jingnan.si@gmail.com>
2022-04-25 09:36:59 -0700

    hook page up down in preview window

--- ui/list_with_preview_content_window.py
+++ ui/list_with_preview_content_window.py
@@ -1,6 +1,8 @@
 import logging
 
 from PySide6.QtWidgets import QTextEdit, QHBoxLayout
+from PySide6.QtWidgets import QAbstractSlider
+from PySide6.QtGui import QTextCursor
 
 from .list_content_window import ListContentWindow
 
@@ -24,3 +26,25 @@ class ListWithPreviewContentWindow(ListContentWindow):
     layout.addWidget(list_widget)
     layout.addWidget(self.preview_edit_)
     return layout
+
+  def _page_up_down(self, ctx, pageDown):
+    logging.debug('preview window up down')
+
+    pe = self.preview_edit_
+
+    pe.verticalScrollBar().triggerAction(
+        QAbstractSlider.SliderPageStepAdd if pageDown else QAbstractSlider.
+        SliderPageStepSub)
+    cursor = pe.textCursor()
+    moved = cursor.movePosition(
+        QTextCursor.Down if pageDown else QTextCursor.Up,
+        QTextCursor.MoveAnchor,
+        pe.verticalScrollBar().pageStep())
+
+    logging.debug('preview window cursor moved:{}, steps:{}'.format(
+        moved,
+        pe.verticalScrollBar().pageStep()))
+
+    if moved:
+      pe.setTextCursor(cursor)
+      pe.ensureCursorVisible()
