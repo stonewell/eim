489a8d6487400cb215c4a8a60913e092ef8d9be4
Jingnan Si <jingnan.si@gmail.com>
2022-04-25 20:59:46 -0700

    use QStatusBar for mode-line

--- plugins/mode_line_area/mode_line_area.py
+++ plugins/mode_line_area/mode_line_area.py
@@ -1,10 +1,10 @@
 from PySide6.QtCore import Slot, Qt, QRect, QSize, QMargins
-from PySide6.QtGui import QColor, QPainter
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QStatusBar, QLabel
+
 from pubsub import pub
 
 
-class ModeLineArea(QWidget):
+class ModeLineArea(QStatusBar):
 
   def __init__(self, ctx, editor):
     super().__init__(editor)
@@ -13,48 +13,41 @@ class ModeLineArea(QWidget):
 
     self.ctx_.register_editor_viewport_handler(self)
 
-    self.editor_.blockCountChanged[int].connect(
-        self.update_mode_line_area_height)
+    self.setSizeGripEnabled(False)
+
     self.editor_.updateRequest[QRect, int].connect(self.update_mode_line_area)
-    self.update_mode_line_area_height(0)
+
+    l = QLabel()
+    self.addPermanentWidget(l)
+    l.setText('Line Numbers')
+
+    l = QLabel()
+    self.addWidget(l, 0)
+    l.setText('Buffer info xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
+    l = QLabel()
+    self.addWidget(l, 1)
 
   def sizeHint(self):
     return QSize(0, self.mode_line_area_height())
-
-  #def paintEvent(self, event):
-  #  pass
 
   def mode_line_area_height(self):
     space = 4 + self.editor_.fontMetrics().lineSpacing()
     return space
 
   @Slot()
-  def update_mode_line_area_height(self, newBlockCount):
-    pub.sendMessage('viewport_changed')
-
-  @Slot()
   def update_mode_line_area(self, rect, dy):
     self.update_geometry()
 
-    if dy:
-      self.scroll(0, dy)
-    else:
-      height = self.height()
-
-      self.update(0, rect.y(), rect.width(), height)
-
-    if rect.contains(self.editor_.viewport().rect()):
-      self.update_mode_line_area_height(0)
-
   def update_geometry(self):
     current_geometry = self.geometry()
-    cr = self.editor_.contentsRect()
+    cr = self.editor_.contentsRect() - self.editor_.viewportMargins()
+
     height = self.mode_line_area_height()
-    rect = QRect(cr.left(), cr.top(), cr.width(), height)
+    rect = QRect(cr.left(), cr.bottom(), cr.width(), height)
 
     if current_geometry != rect:
       self.setGeometry(rect)
-      self.update_mode_line_area_height(0)
 
   def get_editor_margin(self):
     return QMargins(0, 0, 0, self.mode_line_area_height())
