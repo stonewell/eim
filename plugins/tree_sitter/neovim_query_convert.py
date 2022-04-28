1d7aea0584ffc59c8374ab28cee5edca82a3d58e
Jingnan Si <jingnan.si@gmail.com>
2022-04-27 21:35:07 -0700

    handle comment more robust nvim query converter

--- plugins/tree_sitter/neovim_query_convert.py
+++ plugins/tree_sitter/neovim_query_convert.py
@@ -5,11 +5,9 @@ import pathlib
 
 def convert(content):
   v = content
-  v = re.sub(';;\\s+.*$', '', v, flags=re.MULTILINE)
-  v = re.sub(';\\s+.*$', '', v, flags=re.MULTILINE)
-  v = re.sub('\\s+', ' ', v, flags=re.MULTILINE)
+  v = re.sub('[ \\t\\f\\v]+', ' ', v, flags=re.MULTILINE)
 
-  vv = re.split(r'([()" \\])', v)
+  vv = re.split(r'([()" \\;\r\n])', v)
 
   if v != ''.join(vv):
     raise ValueError()
@@ -17,6 +15,7 @@ def convert(content):
   in_string = False
   in_anyof = False
   in_contains = False
+  in_comment = False
   back_slash = False
 
   new_vv = []
@@ -27,6 +26,12 @@ def convert(content):
 
     if entry == '(':
       pass
+    elif entry == ';':
+      if not in_string:
+        in_comment = True
+    elif entry == '\r' or entry == '\n':
+      if not in_string:
+        in_comment = False
     elif entry == ')':
       if not in_string:
         if in_anyof:
@@ -69,7 +74,7 @@ def convert(content):
       if in_string:
         if entry != '"' or (entry == '"' and back_slash):
           match_value += entry
-      elif entry != '"':
+      elif not entry in ['"'] and not in_comment:
         new_vv.append(entry)
     else:
       new_vv.append(entry)
@@ -82,7 +87,9 @@ def convert(content):
     else:
       back_slash = False
 
-  return ''.join(new_vv)
+  v = ''.join(new_vv)
+
+  return re.sub('^\\s+$', '', v, flags=re.MULTILINE)
 
 if __name__ == '__main__':
   a = pathlib.Path(r'/home/stone/.config/eim/neovim_tree_sitter/data/./queries/python/highlights.scm')
